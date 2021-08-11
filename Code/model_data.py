42f0ff992c91212789eba9171e87073bf7bfc839
Daniel A. Yahalomi <danielyahalomi@gmail.com>
2021-08-11 12:29:05 -0400

    push code

--- Code/model_data.py
+++ Code/model_data.py
@@ -251,7 +251,7 @@ def model_both(rv_map_soln, x_rv, y_rv, y_rv_err, x_astrometry, ra_data, ra_err,
 			m_planet_1_fixed = pm.Potential("m_planet_1_fixed", tt.switch(m_planet_1 > 10., 0, -np.inf))
 			m_planet_2_fixed = pm.Potential("m_planet_2_fixed", tt.switch(m_planet_2 < 10., 0, -np.inf))
 			
-			m_planet = pm.Deterministic("m_planet", np.array([m_planet_1_fixed, m_planet_2_fixed]))
+			m_planet = pm.Deterministic("m_planet", tt.concatenate([m_planet_1_fixed, m_planet_2_fixed]))
 			m_planet_fit = pm.Deterministic("m_planet_fit", m_planet/m_sun)
 			
 			incl = pm.Deterministic("incl", tt.arctan2(sqrtm_sini, sqrtm_cosi))

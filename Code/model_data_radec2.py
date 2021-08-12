2f20903f81e9b5c6aa76447c74e046d211d12bb5
Daniel A. Yahalomi <danielyahalomi@gmail.com>
2021-08-12 05:39:09 -0400

    push code

--- Code/model_data_radec2.py
+++ Code/model_data_radec2.py
@@ -385,9 +385,9 @@ def model_both(rv_map_soln, x_rv, y_rv, y_rv_err, x_astrometry, ra_data, ra_err,
 
 			# Optimize to find the initial parameters
 			map_soln = model.test_point
-			map_soln = pmx.optimize(map_soln, vars=[m_planet, incl])
-			map_soln = pmx.optimize(map_soln, vars=[m_planet, incl, Omega])
-			map_soln = pmx.optimize(map_soln, vars=[Omega, ecs])
+			map_soln = pmx.optimize(map_soln, vars=[m_planet, incl, phase])
+			map_soln = pmx.optimize(map_soln, vars=[m_planet, incl, Omega, phase])
+			map_soln = pmx.optimize(map_soln, vars=[Omega, ecs, phase])
 			map_soln = pmx.optimize(map_soln, vars=[P, a, phase])
 			map_soln = pmx.optimize(map_soln)
 

c1cc4ec925123c040a4575660d57f99bd2d100da
Daniel A. Yahalomi <danielyahalomi@gmail.com>
2021-08-25 16:36:17 -0400

    push code

--- Code/model.py
+++ Code/model.py
@@ -85,6 +85,7 @@ def model_rv(periods, Ks, x_rv, y_rv, y_rv_err):
 		map_soln = model.test_point
 		map_soln = pmx.optimize(start=map_soln, vars=[tperi])
 		map_soln = pmx.optimize(start=map_soln, vars=[P])
+		map_soln = pmx.optimize(start=map_soln, vars=[ecs])
 		map_soln = pmx.optimize(start=map_soln)
 
 
@@ -172,192 +173,194 @@ def model_both(rv_map_soln, x_rv, y_rv, y_rv_err, x_astrometry, ra_data, ra_err,
 	# for predicted orbits
 	t_fine = np.linspace(x_astrometry.min() - 500, x_astrometry.max() + 500, num=1000)
 
-
-	def get_model():
-		with pm.Model() as model:
-
-
-			# Below we will run a version of this model where a measurement of parallax is provided
-			# The measurement is in milliarcsec
-			m_plx = pm.Bound(pm.Normal, lower=0, upper=200)(
-				"m_plx", mu=parallax*1000, sd=10, testval=parallax*1000
-			)
-			plx = pm.Deterministic("plx", 1e-3 * m_plx)
+	inc_test_vals = np.array(np.radians([0.01, 10., 20., 30., 40., 50., 60., 70., 80., 89.9]))
+	model, map_soln = [], []
+	for inc in inc_test_vals:
 
 
-			# We expect the period to be around that found from just the RVs,
-			# so we'll set a broad prior on logP
-			
-			logP = pm.Uniform(
-				"logP", lower=0, upper=np.log(10000.), testval=np.log(P_RV), shape=2
-			)
-			P = pm.Deterministic("P", tt.exp(logP))
-			
-			# Eccentricity & argument of periasteron
-			ecs = pmx.UnitDisk("ecs", shape=(2, 2), 
-							   testval=np.array([np.sqrt(ecc_RV)*np.cos(omega_RV), 
-												 np.sqrt(ecc_RV)*np.sin(omega_RV)]))
-			ecc = pm.Deterministic("ecc", tt.sum(ecs ** 2, axis=0))
-			omega = pm.Deterministic("omega", tt.arctan2(ecs[1], ecs[0]))
-			
-			
-
-			# Omegas are co-dependent, so sample them with variables Omega_plus
-			# and Omegas_minus. Omega_plus is (Omega_0 + Omega_1)/2 and 
-			# Omega_minus is (Omega_0 - Omega_1)/2
-			
-			Omega_plus = pmx.Angle("Omega_plus", shape=1)
-			Omega_minus = pmx.Angle("Omega_minus", shape=1)
-			
-			
-			Omega = tt.concatenate( [(Omega_plus + Omega_minus),
-									 (Omega_plus - Omega_minus)] )
-			
-
-			Omega = pm.Deterministic("Omega", Omega) 
-			Omega_sum = pm.Deterministic("Omega_sum", ((Omega_plus)*2)% np.pi)
-			Omega_diff = pm.Deterministic("Omega_diff", ((Omega_minus)*2)% np.pi)
-			
+		def get_model():
+			with pm.Model() as model:
 
 
-		
-			# For these orbits, it can also be better to fit for a phase angle
-			# (relative to a reference time) instead of the time of periasteron
-			# passage directly
-			phase = pmx.Angle("phase", testval=phase_RV, shape=2)
-			tperi = pm.Deterministic("tperi", P * phase / (2 * np.pi))
-			
+				# Below we will run a version of this model where a measurement of parallax is provided
+				# The measurement is in milliarcsec
+				m_plx = pm.Bound(pm.Normal, lower=0, upper=200)(
+					"m_plx", mu=parallax*1000, sd=10, testval=parallax*1000
+				)
+				plx = pm.Deterministic("plx", 1e-3 * m_plx)
 
 
-			inc_test_vals = np.array([0., 10., 20., 30., 40., 50., 60., 70., 80., 90.])
-			mass_test_vals = min_masses_RV/inc_test_vals
-
-
-			# uniform prior on sqrtm_sini and sqrtm_cosi (upper 10* min mass to stop planet flipping)
-			sqrtm_sini = pm.Uniform(
-				"sqrtm_sini_1", lower=0, upper=100*min_mass(K_RV, P_RV, ecc_RV)*m_sun, 
-				testval = min_mass(K_RV, P_RV, ecc_RV)*m_sun, shape=2)
-			
-			sqrtm_cosi = pm.Uniform(
-				"sqrtm_cosi_1", lower=0, upper=100*min_mass(K_RV, P_RV, ecc_RV)*m_sun, 
-				testval = min_mass(K_RV, P_RV, ecc_RV)*m_sun, shape=2)
-
-		
-			m_planet = pm.Deterministic("m_planet", sqrtm_sini**2. + sqrtm_cosi**2.)
-			m_planet_fit = pm.Deterministic("m_planet_fit", m_planet/m_sun)
-			
-			incl = pm.Deterministic("incl", tt.arctan2(sqrtm_sini, sqrtm_cosi))
-			
-			# add keplers 3 law function
-			a = pm.Deterministic("a", a_from_Kepler3(P, 1.0+m_planet_fit))
-			
-			# Set up the orbit
-			orbit = xo.orbits.KeplerianOrbit(
-				t_periastron=tperi,
-				period=P,
-				incl=incl,
-				ecc=ecc,
-				omega=omega,
-				Omega=Omega,
-				m_planet = m_planet_fit,
-				plx=plx
-			)
-
-
-			
-			
-			# Add a function for computing the full astrometry model
-			def get_astrometry_model(t, name=""):
-				# First the astrometry induced by the planets
-
-				# determine and print the star position at desired times
-				pos = orbit.get_star_position(t, plx)
-
-				x,y,z = pos
-
-
-				# calculate rho and theta
-				rhos = tt.squeeze(tt.sqrt(x ** 2 + y ** 2))  # arcsec
-				thetas = tt.squeeze(tt.arctan2(y, x))  # radians between [-pi, pi]
-								
+				# We expect the period to be around that found from just the RVs,
+				# so we'll set a broad prior on logP
 				
-				#rhos, thetas = get_star_relative_angles(t, plx)
+				logP = pm.Uniform(
+					"logP", lower=0, upper=np.log(10000.), testval=np.log(P_RV), shape=2
+				)
+				P = pm.Deterministic("P", tt.exp(logP))
+				
+				# Eccentricity & argument of periasteron
+				ecs = pmx.UnitDisk("ecs", shape=(2, 2), 
+								   testval=np.array([np.sqrt(ecc_RV)*np.cos(omega_RV), 
+													 np.sqrt(ecc_RV)*np.sin(omega_RV)]))
+				ecc = pm.Deterministic("ecc", tt.sum(ecs ** 2, axis=0))
+				omega = pm.Deterministic("omega", tt.arctan2(ecs[1], ecs[0]))
 				
 				
-				dec = pm.Deterministic("dec" + name, rhos * np.cos(thetas)) # X is north
-				ra = pm.Deterministic("ra" + name, rhos * np.sin(thetas)) # Y is east
+
+				# Omegas are co-dependent, so sample them with variables Omega_plus
+				# and Omegas_minus. Omega_plus is (Omega_0 + Omega_1)/2 and 
+				# Omega_minus is (Omega_0 - Omega_1)/2
 				
-				# Sum over planets to get the full model
-				dec_model = pm.Deterministic("dec_model" + name, tt.sum(dec, axis=-1))
-				ra_model = pm.Deterministic("ra_model" + name, tt.sum(ra, axis=-1))
+				Omega_plus = pmx.Angle("Omega_plus", shape=1)
+				Omega_minus = pmx.Angle("Omega_minus", shape=1)
+				
+				
+				Omega = tt.concatenate( [(Omega_plus + Omega_minus),
+										 (Omega_plus - Omega_minus)] )
 				
 
+				Omega = pm.Deterministic("Omega", Omega) 
+				Omega_sum = pm.Deterministic("Omega_sum", ((Omega_plus)*2)% np.pi)
+				Omega_diff = pm.Deterministic("Omega_diff", ((Omega_minus)*2)% np.pi)
 				
-				return dec_model, ra_model
-
-			
-			# Define the astrometry model at the observed times
-			dec_model, ra_model = get_astrometry_model(x_astrometry)
-
-			# Also define the model on a fine grid as computed above (for plotting)
-			dec_model_fine, ra_model_fine = get_astrometry_model(t_fine, name="_fine")
-
-			
-
-			# Add jitter terms to both separation and position angle
-			log_dec_s = pm.Normal(
-				"log_dec_s", mu=np.log(np.median(dec_err)), sd=dec_err
-			)
-			log_ra_s = pm.Normal(
-				"log_ra_s", mu=np.log(np.median(ra_err)), sd=ra_err
-			)
-			dec_tot_err = tt.sqrt(dec_err ** 2 + tt.exp(2 * log_dec_s))
-			ra_tot_err = tt.sqrt(ra_err ** 2 + tt.exp(2 * log_ra_s))
-
-			# define the likelihood function, e.g., a Gaussian on both ra and dec		
-			pm.Normal("dec_obs", mu=dec_model, sd=dec_tot_err, observed=dec_data)
-			pm.Normal("ra_obs", mu=ra_model, sd=ra_tot_err, observed=ra_data)
 
 
 			
+				# For these orbits, it can also be better to fit for a phase angle
+				# (relative to a reference time) instead of the time of periasteron
+				# passage directly
+				phase = pmx.Angle("phase", testval=phase_RV, shape=2)
+				tperi = pm.Deterministic("tperi", P * phase / (2 * np.pi))
+				
+
+				mass_test_vals = min_masses_RV/np.sin(inc)
+				# uniform prior on sqrtm_sini and sqrtm_cosi (upper 10* min mass to stop planet flipping)
+				sqrtm_sini = pm.Uniform(
+					"sqrtm_sini_1", lower=0, upper=100*min_masses_RV*m_sun, 
+					testval = np.sqrt(mass_test_vals*m_sun)*np.sin(inc), shape=2)
+				
+				sqrtm_cosi = pm.Uniform(
+					"sqrtm_cosi_1", lower=0, upper=100*min_masses_RV*m_sun, 
+					testval = np.sqrt(mass_test_vals*m_sun)*np.cos(inc), shape=2)
+
 			
-			# ADD RV MODEL
-			# Jitter & a quadratic RV trend
-			log_rv = pm.Normal("log_rv", mu=np.log(np.median(y_rv_err)), sd=y_rv_err)
+				m_planet = pm.Deterministic("m_planet", sqrtm_sini**2. + sqrtm_cosi**2.)
+				m_planet_fit = pm.Deterministic("m_planet_fit", m_planet/m_sun)
+				
+				incl = pm.Deterministic("incl", tt.arctan2(sqrtm_sini, sqrtm_cosi))
+				
+				# add keplers 3 law function
+				a = pm.Deterministic("a", a_from_Kepler3(P, 1.0+m_planet_fit))
+				
+				# Set up the orbit
+				orbit = xo.orbits.KeplerianOrbit(
+					t_periastron=tperi,
+					period=P,
+					incl=incl,
+					ecc=ecc,
+					omega=omega,
+					Omega=Omega,
+					m_planet = m_planet_fit,
+					plx=plx
+				)
 
 
-			# And a function for computing the full RV model
-			def get_rv_model(t, name=""):
-				# First the RVs induced by the planets
-				vrad = orbit.get_radial_velocity(t)
-				pm.Deterministic("vrad" + name, vrad)
+				
+				
+				# Add a function for computing the full astrometry model
+				def get_astrometry_model(t, name=""):
+					# First the astrometry induced by the planets
 
-				# Sum over planets to get the full model
-				return pm.Deterministic("rv_model" + name, tt.sum(vrad, axis=-1))
+					# determine and print the star position at desired times
+					pos = orbit.get_star_position(t, plx)
 
-			# Define the RVs at the observed times
-			rv_model = get_rv_model(x_rv)
-
-			# Also define the model on a fine grid as computed above (for plotting)
-			rv_model_pred = get_rv_model(t_rv, name="_pred")
-
-			# Finally add in the observation model. This next line adds a new contribution
-			# to the log probability of the PyMC3 model
-			rv_err = tt.sqrt(y_rv_err ** 2 + tt.exp(2 * log_rv))
-			pm.Normal("obs_RV", mu=rv_model, sd=rv_err, observed=y_rv)
-
-			# Optimize to find the initial parameters
-			map_soln = model.test_point
-			map_soln = pmx.optimize(map_soln, vars=[m_planet, incl, Omega])
-			map_soln = pmx.optimize(map_soln, vars=[P, phase])
-			map_soln = pmx.optimize(map_soln)
+					x,y,z = pos
 
 
-		return model, map_soln
+					# calculate rho and theta
+					rhos = tt.squeeze(tt.sqrt(x ** 2 + y ** 2))  # arcsec
+					thetas = tt.squeeze(tt.arctan2(y, x))  # radians between [-pi, pi]
+									
+					
+					#rhos, thetas = get_star_relative_angles(t, plx)
+					
+					
+					dec = pm.Deterministic("dec" + name, rhos * np.cos(thetas)) # X is north
+					ra = pm.Deterministic("ra" + name, rhos * np.sin(thetas)) # Y is east
+					
+					# Sum over planets to get the full model
+					dec_model = pm.Deterministic("dec_model" + name, tt.sum(dec, axis=-1))
+					ra_model = pm.Deterministic("ra_model" + name, tt.sum(ra, axis=-1))
+					
+
+					
+					return dec_model, ra_model
+
+				
+				# Define the astrometry model at the observed times
+				dec_model, ra_model = get_astrometry_model(x_astrometry)
+
+				# Also define the model on a fine grid as computed above (for plotting)
+				dec_model_fine, ra_model_fine = get_astrometry_model(t_fine, name="_fine")
+
+				
+
+				# Add jitter terms to both separation and position angle
+				log_dec_s = pm.Normal(
+					"log_dec_s", mu=np.log(np.median(dec_err)), sd=dec_err
+				)
+				log_ra_s = pm.Normal(
+					"log_ra_s", mu=np.log(np.median(ra_err)), sd=ra_err
+				)
+				dec_tot_err = tt.sqrt(dec_err ** 2 + tt.exp(2 * log_dec_s))
+				ra_tot_err = tt.sqrt(ra_err ** 2 + tt.exp(2 * log_ra_s))
+
+				# define the likelihood function, e.g., a Gaussian on both ra and dec		
+				pm.Normal("dec_obs", mu=dec_model, sd=dec_tot_err, observed=dec_data)
+				pm.Normal("ra_obs", mu=ra_model, sd=ra_tot_err, observed=ra_data)
+
+
+				
+				
+				# ADD RV MODEL
+				# Jitter & a quadratic RV trend
+				log_rv = pm.Normal("log_rv", mu=np.log(np.median(y_rv_err)), sd=y_rv_err)
+
+
+				# And a function for computing the full RV model
+				def get_rv_model(t, name=""):
+					# First the RVs induced by the planets
+					vrad = orbit.get_radial_velocity(t)
+					pm.Deterministic("vrad" + name, vrad)
+
+					# Sum over planets to get the full model
+					return pm.Deterministic("rv_model" + name, tt.sum(vrad, axis=-1))
+
+				# Define the RVs at the observed times
+				rv_model = get_rv_model(x_rv)
+
+				# Also define the model on a fine grid as computed above (for plotting)
+				rv_model_pred = get_rv_model(t_rv, name="_pred")
+
+				# Finally add in the observation model. This next line adds a new contribution
+				# to the log probability of the PyMC3 model
+				rv_err = tt.sqrt(y_rv_err ** 2 + tt.exp(2 * log_rv))
+				pm.Normal("obs_RV", mu=rv_model, sd=rv_err, observed=y_rv)
+
+				# Optimize to find the initial parameters
+				map_soln = model.test_point
+				map_soln = pmx.optimize(map_soln, vars=[m_planet, incl, Omega])
+				map_soln = pmx.optimize(map_soln, vars=[P, phase])
+				map_soln = pmx.optimize(map_soln)
+
+
+			return model, map_soln
 
 
 
-	model, map_soln = get_model()
+		a_model, a_map_soln = get_model()
+		model.append(a_model)
+		map_soln.append(a_map_soln)
 
 	return model, map_soln
 

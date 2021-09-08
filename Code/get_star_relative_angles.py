9ff762273553d5f7b7d477006fee087406efd609
Daniel A. Yahalomi <danielyahalomi@gmail.com>
2021-09-08 17:27:11 -0400

    push code

--- Code/get_star_relative_angles.py
+++ Code/get_star_relative_angles.py
@@ -1,42 +1,25 @@
-def get_star_relative_angles(t, parallax):
-    
-    # determine and print the star position at desired times
-    pos = theano.function([], orbit.get_star_position(t, parallax))()
-
-
-    #pos = tt.sum(pos, axis=-1)
-
-    x,y,z = pos
-
+def get_star_relative_angles(self, t, parallax=None, light_delay=False):
+    """The stars' relative position to the star in the sky plane, in
+    separation, position angle coordinates.
+    .. note:: This treats each planet independently and does not take the
+        other planets into account when computing the position of the
+        star. This is fine as long as the planet masses are small.
+    Args:
+        t: The times where the position should be evaluated.
+        light_delay: account for the light travel time delay? Default is
+            False.
+    Returns:
+        The separation (arcseconds) and position angle (radians,
+        measured east of north) of the planet relative to the star.
+    """
+    X, Y, Z = self._get_star_position(
+        -self.a, t, parallax, light_delay=light_delay
+    )
 
     # calculate rho and theta
-    rho = tt.squeeze(tt.sqrt(x ** 2 + y ** 2))  # arcsec
-    theta = tt.squeeze(tt.arctan2(y, x))  # radians between [-pi, pi]
-    
-    rho, theta = rho.eval(), theta.eval()
-    
-    return rho, theta
+    rho = tt.squeeze(tt.sqrt(X ** 2 + Y ** 2))  # arcsec
+    theta = tt.squeeze(tt.arctan2(Y, X))  # radians between [-pi, pi]
+
+    return (rho, theta)
 
 
-rho, theta = get_star_relative_angles(x_astrometry, parallax)
-
-
-ra = rho * np.sin(theta)
-dec = rho * np.cos(theta)
-
-#ra_single = ra.eval()
-#dec_single = dec.eval()
-
-ra_orbit_sum = tt.sum(ra, axis=-1)
-dec_orbit_sum = tt.sum(dec, axis=-1)
-
-ra_orbit_sum = ra_orbit_sum.eval()
-dec_orbit_sum = dec_orbit_sum.eval()
-
-
-fig, ax = plt.subplots(2, 1, figsize = (15,15))
-
-
-ax[0].plot(x_astrometry, ra_orbit_sum, 'o')
-
-ax[1].plot(x_astrometry, dec_orbit_sum, 'o')
